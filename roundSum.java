f6d2c0d4017fca332d94f4e56509d2f9803ec73d
Aczia <Tuan.Fischer@Student.HTW-Berlin.de>
2021-04-20 13:37:26 +0200

    roundSum Exercise for Nina

---
+++ roundSum.java
@@ -0,0 +1,16 @@
+
+public class roundSum {
+
+}
+public int roundSum(int a, int b, int c) {
+	  int sum = // Your turn: the sum of the rounded integers
+	  return sum;
+	}
+
+	public int round10(int num){ //a helper to avoid repetition in the code before
+	  if(num% "SomethingIsNeededHere"){ //Your turn: round up if rightmost number is 5 or greater 
+	    return "ReplaceThis"; //Your turn: how do we get the next multiple of 10 from num
+	  }else{ // if we do not round up we do else ->
+	    return "ReplaceThis";// Your turn: how do we get the multiple of 10 before num
+	  }
+	}
