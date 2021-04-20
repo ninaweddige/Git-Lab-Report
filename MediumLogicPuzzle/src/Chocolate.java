3df45bc176a86f4102847549567c6a59fc3baecb
Nina Weddige <s0578004@htw-berlin.de>
2021-04-20 13:41:44 +0200

    makeChocolate exercise for Dai

---
+++ MediumLogicPuzzle/src/Chocolate.java
@@ -0,0 +1,22 @@
+public class Chocolate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	/*This method makes a package of chocolate out of big and small
+	 * chocolate bars, if possible. "Goal" is the size of the
+	 * package to be made in kilograms, "small" is the amount of small
+	 * chocolate bars and "big" is the amount of large chocolate bars
+	 * available to make the package. Small bars are 1 kg and big
+	 * bars are 5kg. The big bars are used before the small bars
+	 * and the method returns the number of small bars to use, or -1
+	 * if it's not possible.
+	 */
+	
+	public int makeChocolate(int small, int big, int goal) {
+		//method
+	}
+
+}
